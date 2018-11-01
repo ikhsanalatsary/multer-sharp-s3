e5f3faffaceda4da183c9096dc1553662d610838
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-11-01 14:05:30 +0700

    rebuild

--- dist/main.js
+++ dist/main.js
@@ -17,8 +17,11 @@ class S3Storage {
         if (!options.s3) {
             throw new Error('You have to specify bucket for AWS S3 to work.');
         }
-        if (!options.Key || typeof options.Key !== 'string') {
+        if (!options.Key) {
             this._getKey = get_filename_1.default;
+        }
+        else if (typeof options.Key === 'function') {
+            this._getKey = options.Key;
         }
         this.opts = options;
         this.sharpOpts = get_sharp_options_1.default(options);
@@ -26,36 +29,18 @@ class S3Storage {
     _handleFile(req, file, cb) {
         const { opts, sharpOpts } = this;
         const { stream } = file;
-        if (!opts.Key || typeof opts.Key !== 'string') {
+        if (typeof this._getKey === 'function') {
             this._getKey(req, file, (fileErr, Key) => {
-                let params = {
-                    Bucket: opts.Bucket,
-                    ACL: opts.ACL,
-                    CacheControl: opts.CacheControl,
-                    ContentType: opts.ContentType,
-                    Metadata: opts.Metadata,
-                    StorageClass: opts.StorageClass,
-                    ServerSideEncryption: opts.ServerSideEncryption,
-                    SSEKMSKeyId: opts.SSEKMSKeyId,
-                    Body: stream,
-                    Key,
-                };
+                if (fileErr) {
+                    cb(fileErr);
+                    return;
+                }
+                let params = { Bucket: opts.Bucket, ACL: opts.ACL, CacheControl: opts.CacheControl, ContentType: opts.ContentType, Metadata: opts.Metadata, StorageClass: opts.StorageClass, ServerSideEncryption: opts.ServerSideEncryption, SSEKMSKeyId: opts.SSEKMSKeyId, Body: stream, Key };
                 this._uploadProcess(params, stream, cb);
             });
         }
         else {
-            const params = {
-                Bucket: opts.Bucket,
-                ACL: opts.ACL,
-                CacheControl: opts.CacheControl,
-                ContentType: opts.ContentType,
-                Metadata: opts.Metadata,
-                StorageClass: opts.StorageClass,
-                ServerSideEncryption: opts.ServerSideEncryption,
-                SSEKMSKeyId: opts.SSEKMSKeyId,
-                Body: stream,
-                Key: opts.Key,
-            };
+            const params = { Bucket: opts.Bucket, ACL: opts.ACL, CacheControl: opts.CacheControl, ContentType: opts.ContentType, Metadata: opts.Metadata, StorageClass: opts.StorageClass, ServerSideEncryption: opts.ServerSideEncryption, SSEKMSKeyId: opts.SSEKMSKeyId, Body: stream, Key: opts.Key };
             this._uploadProcess(params, stream, cb);
         }
     }
@@ -117,21 +102,7 @@ class S3Storage {
             sizes
                 .pipe(resizeWithSharp, getContentType, uploadToAws, operators_1.toArray())
                 .subscribe((res) => {
-                const mapArrayToObject = res.reduce((acc, curr) => {
-                    acc[curr.suffix] = {};
-                    acc[curr.suffix].Location = curr.Location;
-                    acc[curr.suffix].Key = curr.Key;
-                    acc[curr.suffix].Size = curr.currentSize;
-                    acc[curr.suffix].Bucket = curr.Bucket;
-                    acc[curr.suffix].ACL = opts.ACL;
-                    acc[curr.suffix].ContentType = opts.ContentType || curr.ContentType;
-                    acc[curr.suffix].ContentDisposition = opts.ContentDisposition;
-                    acc[curr.suffix].StorageClass = opts.StorageClass;
-                    acc[curr.suffix].ServerSideEncryption = opts.ServerSideEncryption;
-                    acc[curr.suffix].Metadata = opts.Metadata;
-                    acc[curr.suffix].ETag = curr.ETag;
-                    return acc;
-                }, {});
+                const mapArrayToObject = res.reduce(this._iterator.bind(this), {});
                 cb(null, mapArrayToObject);
             }, cb);
         }
@@ -141,32 +112,56 @@ class S3Storage {
             params.Body = stream.pipe(resizerStream);
             const meta = { stream: params.Body };
             const meta$ = rxjs_1.from(meta.stream.metadata());
-            meta$.subscribe((metadata) => {
+            meta$
+                .pipe(operators_1.map((metadata) => {
                 params.ContentType = opts.ContentType || metadata.format;
+                return metadata;
+            }), operators_1.mergeMap((metadata) => {
                 const upload = opts.s3.upload(params);
                 upload.on('httpUploadProgress', function (ev) {
                     if (ev.total) {
                         currentSize = ev.total;
                     }
                 });
-                const upload$ = rxjs_1.from(upload.promise());
-                upload$.subscribe((result) => {
-                    cb(null, {
-                        Size: currentSize,
-                        Bucket: opts.Bucket,
-                        ACL: opts.ACL,
-                        ContentType: opts.ContentType || metadata.format,
-                        ContentDisposition: opts.ContentDisposition,
-                        StorageClass: opts.StorageClass,
-                        ServerSideEncryption: opts.ServerSideEncryption,
-                        Metadata: opts.Metadata,
-                        Location: result.Location,
-                        ETag: result.ETag,
-                        Key: result.Key,
-                    });
-                }, cb);
+                const upload$ = rxjs_1.from(new Promise((resolve, reject) => {
+                    upload.promise().then((res) => {
+                        resolve(Object.assign({}, res, { format: metadata.format }));
+                    }, reject);
+                }));
+                return upload$;
+            }))
+                .subscribe((result) => {
+                cb(null, {
+                    Size: currentSize,
+                    Bucket: opts.Bucket,
+                    ACL: opts.ACL,
+                    ContentType: opts.ContentType || result.format,
+                    ContentDisposition: opts.ContentDisposition,
+                    StorageClass: opts.StorageClass,
+                    ServerSideEncryption: opts.ServerSideEncryption,
+                    Metadata: opts.Metadata,
+                    Location: result.Location,
+                    ETag: result.ETag,
+                    Key: result.Key,
+                });
             }, cb);
         }
+    }
+    _iterator(acc, curr) {
+        const { opts } = this;
+        acc[curr.suffix] = {};
+        acc[curr.suffix].Location = curr.Location;
+        acc[curr.suffix].Key = curr.Key;
+        acc[curr.suffix].Size = curr.currentSize;
+        acc[curr.suffix].Bucket = curr.Bucket;
+        acc[curr.suffix].ACL = opts.ACL;
+        acc[curr.suffix].ContentType = opts.ContentType || curr.ContentType;
+        acc[curr.suffix].ContentDisposition = opts.ContentDisposition;
+        acc[curr.suffix].StorageClass = opts.StorageClass;
+        acc[curr.suffix].ServerSideEncryption = opts.ServerSideEncryption;
+        acc[curr.suffix].Metadata = opts.Metadata;
+        acc[curr.suffix].ETag = curr.ETag;
+        return acc;
     }
 }
 function s3Storage(options) {
