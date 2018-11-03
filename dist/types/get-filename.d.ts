bc25d96924d6a62e86203761a94b826cdc3349a8
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-11-03 23:52:39 +0700

    delete dist folder

--- dist/types/get-filename.d.ts
+++
@@ -1,3 +0,0 @@
-declare function getFileName(req: any, file: any, cb: any): void;
-export default getFileName;
-//# sourceMappingURL=get-filename.d.ts.map
--- dist/types/get-sharp-options.d.ts
+++
@@ -1,4 +0,0 @@
-import { S3StorageOptions, SharpOptions } from './types';
-declare function getSharpOptions(options: S3StorageOptions): SharpOptions;
-export default getSharpOptions;
-//# sourceMappingURL=get-sharp-options.d.ts.map
